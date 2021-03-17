9bca9cd73cf0db8ffb08eba51e155465f4fb1e06
Peter Pan <soeschroe@gmail.com>
2021-03-17 17:38:56 +0100

    Update Precast.js
    Update fire golem casting

--- kolbot/libs/common/Precast.js
+++ kolbot/libs/common/Precast.js
@@ -422,11 +422,33 @@ var Precast = new function () {
 				else {
 					count = 1;
 					
-				}							
+				}	
+				break;
 			case 85: // Blood Golem
 			case 94: // Fire Golem
+				var mastery = me.getSkill(79, 1);
 				minion = 3;
-
+				if (mastery >= 5 && mastery < 10 ) {
+					count = 2;
+					
+				}
+				else if (mastery >= 10 && mastery < 15) {
+					count = 3;
+					
+				}
+				else if (mastery >= 15 && mastery < 20) {
+					count = 4;
+					
+				}
+				else if (mastery >= 20) {
+					count = 5;
+					
+				}	
+				else {
+					count = 1;
+					
+				}
+			
 				break;
 			case 221: // Raven
 				minion = 10;
